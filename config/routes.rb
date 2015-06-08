687d34822f7cc1fe0be204eedf97d8d62ba1b3fb
CAV <cavpollo@gmail.com>
2015-06-07 20:30:08 -0600

    Screen webstats working

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'webstats' => 'webstats#index'
   get 'webstats/art' => 'webstats#art'
   get 'webstats/modes' => 'webstats#modes'
+  get 'webstats/screen' => 'webstats#screen'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
