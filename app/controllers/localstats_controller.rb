ddeb9d1ce7c28dc83550c59483774cebc0c302c4
Carlos Villagran <cavpollo@gmail.com>
2016-03-06 02:55:52 -0600

    Nice localchart

--- app/controllers/localstats_controller.rb
+++ app/controllers/localstats_controller.rb
@@ -11,12 +11,16 @@ class LocalstatsController < ApplicationController
 
   def devices_data
     # SELECT COUNT(*) AS count_all, device_brand AS device_brand FROM "unique_devices" GROUP BY "unique_devices"."device_brand"  ORDER BY count_all desc
-    brands = UniqueDevice.group(:device_brand).order(:device_brand).count()
+    min_brand_count = 10
+    brands_count = UniqueDevice.group(:device_brand).order(:device_brand).count()
+    brands = brands_count.reject { |key, value| value <= min_brand_count }
+    brands["Other (less than #{min_brand_count})"] = brands_count.reject { |key, value| value > min_brand_count }.collect{ |key, value| value }.sum
+
     # SELECT COUNT(*) AS count_all, device_model AS device_model FROM "unique_devices" GROUP BY "unique_devices"."device_model"  ORDER BY count_all desc
-    min_count = 10
+    min_model_count = 10
     models_count = UniqueDevice.group(:device_model).order(:device_model).count()
-    models = models_count.reject { |key, value| value <= min_count }
-    models["Other (less than #{min_count})"] = models_count.reject { |key, value| value > min_count }.collect{ |key, value| value }.sum
+    models = models_count.reject { |key, value| value <= min_model_count }
+    models["Other (less than #{min_model_count})"] = models_count.reject { |key, value| value > min_model_count }.collect{ |key, value| value }.sum
 
     respond_to do |format|
       format.html { render :devices }
